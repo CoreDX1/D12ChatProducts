444c0ff8813ab2539a1d44a6c465ba60382f7d14
CoreDX1 <chrismaquis@gmail.com>
2022-05-04 11:32:06 -0300

    Estilos

--- static/index.js
+++ static/index.js
@@ -14,9 +14,9 @@ const render = (data) => {
   const html = data.length > 0 ? data.map((pr) => {
     return `
       <tr>
-        <th scope="row">${pr.name}</th>
-        <td>${pr.price}</td>
-        <td><img style="width:100px;height:100px" src="${pr.image}"></td>
+        <th class="tr">${pr.name}</th>
+        <td class="tr">${pr.price}</td>
+        <td class="tr"><img class="imagen" src="${pr.image}"></td>
       </tr>
     `
   }).join(' ') : `
